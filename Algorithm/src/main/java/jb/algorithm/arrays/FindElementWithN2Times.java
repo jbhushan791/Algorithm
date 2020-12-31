cfb521acba024b52d0955237ac7db61ea8f573da
jbhushan791 <bhushan.jyoti791@gmail.com>
2020-12-31 02:02:52 -0500

    Mjaority element and find Max difference from the given array

--- Algorithm/src/main/java/jb/algorithm/arrays/FindElementWithN2Times.java
+++ Algorithm/src/main/java/jb/algorithm/arrays/FindElementWithN2Times.java
@@ -1,22 +1,28 @@
 package jb.algorithm.arrays;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @author jbhushan
  */
 public class FindElementWithN2Times {
 
     public static void main(String[] args){
-        int[] arr = {1,1,5,5,5,5,6};
+        int[] arr = {1,5,5,5,5,5,6,6};
         int num = efficient(arr);
+        System.out.println(num);
+
+        num = findMaxElement(arr);
         System.out.println(num);
     }
 
     public static int findMaxElement(int[] arr) {
-        for(int i=0; i<arr.length/2; i++){
-            if(arr[i] == arr[i+((arr.length/2))]){
+        int l = 0;
+        if(arr.length % 2 == 0){
+            l = arr.length/2;
+        } else {
+           l = (arr.length/2) +1;
+        }
+        for(int i=0; i<l; i++){
+            if(arr[i] == arr[i+((arr.length/2))]) {
                 return arr[i];
             }
         }
@@ -25,7 +31,7 @@ public class FindElementWithN2Times {
 
     public static int efficient(int[] arr) {
         int voter = arr[0];
-        int count = 0;
+        int count = 1;
         for(int i=1; i<arr.length; i++){
             if(arr[i] != voter){
                 count--;
@@ -36,7 +42,6 @@ public class FindElementWithN2Times {
                 voter = arr[i];
             }
         }
-
         int c = 0;
         for(int num: arr){
             if(voter == num){
---
+++ Algorithm/src/main/java/jb/algorithm/arrays/FindMaxDifference.java
@@ -0,0 +1,47 @@
+package jb.algorithm.arrays;
+
+/**
+ * @author jbhushan
+ */
+public class FindMaxDifference {
+
+    public static void main(String[] args){
+        int[] arr = {3,1,4,7,5,100,10};
+
+        int num = findMaxDifference(arr);
+        System.out.println(num);
+
+        num = efficient(arr);
+        System.out.println(num);
+    }
+
+    public static int findMaxDifference(int[] arr) {
+        int maxDifference = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i] < arr[j]){
+                    maxDifference = Math.max(maxDifference, arr[j] - arr[i]);
+                }
+            }
+        }
+        return maxDifference;
+    }
+
+    public static int efficient(int[] arr) {
+
+        int[] diffArray = new int[arr.length-1];
+        for(int i=0; i<arr.length-1; i++){
+            diffArray[i] = arr[i+1]-arr[i];
+        }
+
+        int maxDiff = diffArray[0];
+        for(int i=1; i<arr.length-1; i++){
+            if(diffArray[i-1] > 0){
+                diffArray[i] = diffArray[i]+ diffArray[i-1];
+            }
+            maxDiff = Math.max(maxDiff, diffArray[i]);
+        }
+        return maxDiff;
+    }
+
+}
